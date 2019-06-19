45ecccdfe27b2853c2271ec6a4bf61e3bb37edd9
nitish <nitishxp@gmail.com>
2019-06-19 18:21:30 +0530

    fixes

--- entrypoint-scripts/web-entrypoint.sh
+++ entrypoint-scripts/web-entrypoint.sh
@@ -2,4 +2,5 @@
 echo running post install scripts for web..;
 python /code/manage.py makemigrations
 python /code/manage.py migrate
+python /code/manage.py test
 python /code/manage.py runserver 0.0.0.0:8000
